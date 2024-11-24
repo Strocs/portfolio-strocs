10c443bdd2ebc2ea69deec2510f2dacbd7aec567
Strocs <strocsdev@gmail.com>
2024-11-24 20:39:48 -0300

    first commit for new portfolio version

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel/serverless';
+
+import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), sitemap(), react()],
+  output: 'server',
+  adapter: vercel()
+});
