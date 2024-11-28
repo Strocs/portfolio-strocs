4b5b7d613d143aaf81e19c4c3764fd3c4b216fa0
Strocs <strocsdev@gmail.com>
2024-11-28 11:55:31 -0300

    arch(unfinished) improve folder structure for experience section

---
+++ src/sections/portfolio/experience/data/freelance.ts
@@ -0,0 +1,48 @@
+import type { Experience } from '../types/experience'
+
+export const { title, description, from, to }: Experience = {
+  title: 'Desarrollador Freelance',
+  description:
+    'Desarrollo sitios web con alto rendimiento, utilizando Astro como framework principal, me gusta comprender a profundidad las características del producto de los clientes para crear un sitio web que encaje a la perfección con sus necesidades.',
+  from: '2022',
+  to: 'Actualidad',
+}
+
+export const works = [
+  {
+    title: 'LoungeFi',
+    description:
+      '_LoungeFi es una aplicación web para enfocarse en las tareas diarias,  nace de la idea de crear una aplicación sencilla siguiendo la filosofía  de "Si tienes una idea, programala y listo".',
+    repositoryUrl: 'https://github.com/loungefi/loungefi',
+    webUrl: 'https://loungefi.com',
+    slug: '/',
+    active: true,
+  },
+  {
+    title: 'Festival Frijol Mágico',
+    description:
+      '_LoungeFi es una aplicación web para enfocarse en las tareas diarias,  nace de la idea de crear una aplicación sencilla siguiendo la filosofía  de "Si tienes una idea, programala y listo".',
+    repositoryUrl: 'https://github.com/loungefi/loungefi',
+    webUrl: 'https://loungefi.com',
+    slug: '/',
+    active: false,
+  },
+  {
+    title: 'ARC 2024',
+    description:
+      '_LoungeFi es una aplicación web para enfocarse en las tareas diarias,  nace de la idea de crear una aplicación sencilla siguiendo la filosofía  de "Si tienes una idea, programala y listo".',
+    repositoryUrl: 'https://github.com/loungefi/loungefi',
+    webUrl: 'https://loungefi.com',
+    slug: '/',
+    active: true,
+  },
+  {
+    title: 'Relatos de Barrios',
+    description:
+      '_LoungeFi es una aplicación web para enfocarse en las tareas diarias,  nace de la idea de crear una aplicación sencilla siguiendo la filosofía  de "Si tienes una idea, programala y listo".',
+    repositoryUrl: 'https://github.com/loungefi/loungefi',
+    webUrl: 'https://loungefi.com',
+    slug: '/',
+    active: false,
+  },
+]
---
+++ src/sections/portfolio/experience/types/experience.d.ts
@@ -0,0 +1,10 @@
+export interface Experience {
+  title: string
+  description: string
+  company?: string
+  from: string
+  to: string
+}
+
+
+
