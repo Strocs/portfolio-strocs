6acd553315c5eca668079b7fdfa92ecb59a1a73f
Strocs <strocsdev@gmail.com>
2025-01-03 23:51:20 -0300

    TODO: fix scroll jump on page refresh

---
+++ src/features/portfolio/stack/utils/getIconByName.ts
@@ -0,0 +1,33 @@
+interface AstroSVGComponent {
+  default: {
+    src: string
+    width: number
+    height: number
+    format: string
+  }
+}
+
+const logos = import.meta.glob<AstroSVGComponent>(
+  '../../../../assets/icons/stack/*.svg',
+  {
+    eager: true,
+  }
+)
+
+const namedLogos = Object.values(logos).map((logo) => {
+  const name = logo.default.src.match(/(?!=\/)([a-zA-Z0-9]*)(?=\.svg)/g)![0]
+  return {
+    ...logo,
+    name,
+  }
+})
+const getIconByName = (name: string) => {
+  return namedLogos.find(
+    (logo) =>
+      logo.name.toLowerCase() === name.toLowerCase() ||
+      logo.name.includes(name) ||
+      logo.name.toLowerCase() === 'javascript'
+  )?.default
+}
+
+export { getIconByName, namedLogos, logos }
