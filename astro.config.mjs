0ee096efc0476ffd069ed3f580ad4d39485be123
Strocs <strocsdev@gmail.com>
2025-08-02 21:08:17 -0400

    chore: update project configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,30 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 
+import tailwindcss from '@tailwindcss/vite'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), react()],
-  experimental: {
-    svg: true,
-  },
+  integrations: [sitemap(), react()],
+
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
   },
+
   site: 'https://strocs.dev/',
   output: 'static',
+
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
