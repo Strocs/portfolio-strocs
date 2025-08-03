237e396b332d67edf48f4c02c5ee03a5258f2810
Strocs <strocsdev@gmail.com>
2025-08-02 21:08:52 -0400

    refactor: update content configuration

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,23 +1,17 @@
 import { defineCollection } from 'astro:content'
-import { glob } from 'astro/loaders'
-import { ProjectSchema } from '@/content/schemas/project.schema'
-import { projectLoader } from '@/content/adapters/content-loader.adapter'
-import { i18nSchema } from '@/i18n/schemas/i18n.schema'
+import { ExperienceSchema, ProjectsSchema } from '@/features/content/schemas'
 
-const projects = defineCollection({
-  loader: projectLoader('projects'),
-  schema: ProjectSchema,
+const projectCollection = defineCollection({
+  type: 'content',
+  schema: ProjectsSchema,
 })
 
-const lang = defineCollection({
-  loader: glob({
-    pattern: '*.json',
-    base: './src/i18n',
-  }),
-  schema: i18nSchema,
+const experienceCollection = defineCollection({
+  type: 'content',
+  schema: ExperienceSchema,
 })
 
 export const collections = {
-  projects,
-  lang,
+  projects: projectCollection,
+  experience: experienceCollection,
 }
